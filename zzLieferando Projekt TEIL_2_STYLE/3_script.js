bc0ac4d396fa4016fdde339a9c8fc96b814256bc
Simone Scacciante <simon.scacciante@web.de>
2024-03-15 14:46:55 +0100

    14:45

--- zzLieferando Projekt TEIL_2_STYLE/3_script.js
+++ zzLieferando Projekt TEIL_2_STYLE/3_script.js
@@ -2,14 +2,14 @@ let menuArray = [
     {
         'name': 'Classic',
         'recepte': ['mit Vollmilch Schokoladenglasur'],
-        'price': 1.50,
+        'price': 2.00,
         'picture': ['./img/donut/donut_1.png'],
     },
 
     {
         'name': 'Glitter Glam',
         'recepte': ['Glitzernde Glasur, funkelnde Zuckerperlen'],
-        'price': 3.50,
+        'price': 2.50,
         'picture': ['./img/donut/donut_2.png'],
 
     },
@@ -23,21 +23,21 @@ let menuArray = [
     },
 
     {
-        'name': 'Caramel Dream',
+        'name': 'Caramel Dream Donut',
         'recepte': ['Karamellfüllung und eine zarte Karamellglasur'],
         'price': 2.50,
         'picture': './img/donut/donut_4.png',
     },
 
     {
-        'name': 'Berry Blast',
+        'name': 'Berry Blast Donut',
         'recepte': ['Fruchtige Beerenfüllung mit einem Hauch von Zitrus'],
         'price': 2.00,
         'picture': './img/donut/donut_5.png',
     },
     
     {
-        "name": "Peanut Butter",
+        "name": "Peanut Butter Jelly Time",
         "recepte": ["Erdnussbutterfüllung mit einer Schicht Fruchtgelee oben drauf"],
         "price": 3.00,
         "picture": "./img/donut/donut_6.png"
@@ -45,17 +45,19 @@ let menuArray = [
     {
         "name": "Tropical Paradise",
         "recepte": ["Mango- und Passionsfruchtfüllung, garniert mit Kokosraspeln"],
-        "price": 2.50,
+        "price": 3.25,
         "picture": "./img/donut/donut_7.png"
     },
+
+
+
 ]
 
 let orderMenuArray = [];
 let orderAmountArray = [];
 let orderPriceArraySolo = [];
 let orderPriceArrayTotal = [];
-let deliverPrice = 3.50;
-let minimumOrderValue = 10;
+let deliverPrice = 5;
 
 function render() {
     let menuID = document.getElementById('menuID');
@@ -71,12 +73,15 @@ function menuLoad(i) {
 
     menuID.innerHTML += /*html*/`
     <div class="menuStyle"> 
-        <div class="menuStyleSub">
+        <div>
            <h3> ${element['name']} </h3> 
-            <p>${element['recepte']}<p>
+            ${element['recepte']} 
+            <br>
             <b> ${element['price'].toFixed(2)} € </b> <!-- .toFixed(2) lässt zwei hintere Dezimalzahl erscheinen! -->  
             <input class="inputStyle" id="amountID${i}" type="number" min="1" max ="99" value="1">                      
         </div> 
+
+
         <div class="dishInteract"> 
             <img class="cursor dishInteractButtonFromMenu" src="./img/minus.png" onclick="reduceAmount(${i})">
             
@@ -92,13 +97,16 @@ function menuLoad(i) {
 
 
 function showBasketContainer() {
+
     document.getElementById('donutBasketContainerID').innerHTML = /*html*/`
+    
     <div class="firstBasket" id="basketID" >
         <h2 class="headbasket"> Warenkorb </h2>
             <img src="./img/basketBigImg.png" class="basketBigStyleImg">
         <h2> Fülle deinen Warenkorb</h2>
              <span> Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</span>
     </div>
+
     <div class="secondBasket" id="secondBasketWithDeleteID">
         <div id="basketNoticeID"> </div>
         <div id="payButtonID"> </div>
@@ -126,6 +134,7 @@ function addToBasket(index) {
         orderAmountArray[getMenuIndexOf] += amount;
         orderPriceArrayTotal[getMenuIndexOf] = orderAmountArray[getMenuIndexOf] * menuArray[index]['price'];
     }
+
     document.getElementById("amountID" + index).value = 1;
     renderBasket();
     showBasketBill();
@@ -148,7 +157,9 @@ function renderBasket() {
     basket.innerHTML = '';
     for (let indexBasket = 0; indexBasket < orderMenuArray.length; indexBasket++) {
         basket.innerHTML += /*html*/ `
+
         <div class="dishInBasketContainer">
+
             <div class="disnInBasketContainer_2">
                 <div class="dishInBasketHead">  
                     <div>
@@ -206,25 +217,25 @@ function showBasketBill() {
 
 function showBasketNotice(i) {
     let bill = document.getElementById('basketNoticeID');
-    let stillToPay = 0;
-    stillToPay = minimumOrderValue - totalOrderAndDeliverPrice;
-
     bill.innerHTML = /*html*/ `
+
     <div class="basketNoticeContainer"> 
+
         <div class="payOrderStyleNotice">  
-            <div class="payOrderSpan"><span > 1. Der Mindestbestellwert liegt bei ${minimumOrderValue} <span id="checklistOrder_1"> </span> </span></div>
+            <div class="payOrderSpan"><span > 1. Der Mindestbestellwert liegt bei 10€ <span id="checklistOrder_1"> </span> </span></div>
             <div class="payOrderSpan"><span> 2. Kostenloser Versand ab 25€ <span id="checklistFreeDeliver_2"> </span> </span></div>
             <div> 3. Gratis Donut Express T-Shirt ab 40€ </span> <span id="checklistForFree_1"> </span> </span></div>
         </div>
     </div>
     </div>
-    <div class="payOrderStyle_1" id="payOrderID"> 
-        <div> 
-            <span> Benötigter Betrag, um dem Mindestbestellwert zu erreichen</span>
-        </div>
-        <div> 
-            <b class="payOrderStillToPay"> ${stillToPay.toFixed(2)}€</b>
-        </div>
+        <div class="payOrderStyle_1" id="payOrderID"> 
+            <div> 
+                <span> Benötigter Betrag, um dem Mindestbestellwert zu erreichen</span>
+            </div>
+            <div> 
+                <b class="payOrderStillToPay"> 8.50€ </b>
+            </div>
+        </div> 
     </div>
     `;
     calculateBasketNotice();
@@ -240,6 +251,12 @@ function calculateBasketNotice(index) {
         <img class="correctImg" src="./img/correct.png"> `;
         var payOrderIDElement = document.getElementById('payOrderID');
         payOrderIDElement.setAttribute('style', 'background-color: white; margin-bottom: 0px; margin-top: 0px;'); /* Background Style von Bezahlen !!!! */
+
+
+
+
+
+
     }
 
     if (totalOrderAndDeliverPrice >= 25) {
--- zz_script.js
+++ zz_script.js
@@ -1,20 +1,52 @@
 let menuArray = [
     {
-        'name': 'Hamburger',
-        'recepte': ['Tomatensoße', 'Salami', 'Käse', 'Peperoni'],
-        'price': 10,
+        'name': 'Classic',
+        'recepte': ['mit Vollmilch Schokoladenglasur'],
+        'price': 1.50,
+        'picture': ['./img/donut/donut_1.png'],
     },
 
     {
-        'name': 'Carbonara',
-        'recepte': ['Tomatensoße', 'Apfel'],
-        'price': 15,
+        'name': 'Glitter Glam',
+        'recepte': ['Glitzernde Glasur, funkelnde Zuckerperlen'],
+        'price': 3.50,
+        'picture': ['./img/donut/donut_2.png'],
+
     },
 
     {
-        'name': 'Pizza',
-        'recepte': ['Kartoffel', 'Curry', 'Ketchup'],
-        'price': 20,
+        'name': 'Rainbow Delight',
+        'recepte': ['Fruchtige Füllungen und einer Explosion von Regenbogenfarben'],
+        'price': 3.50,
+        'picture': ['./img/donut/donut_3.png'],
+
+    },
+
+    {
+        'name': 'Caramel Dream',
+        'recepte': ['Karamellfüllung und eine zarte Karamellglasur'],
+        'price': 2.50,
+        'picture': './img/donut/donut_4.png',
+    },
+
+    {
+        'name': 'Berry Blast',
+        'recepte': ['Fruchtige Beerenfüllung mit einem Hauch von Zitrus'],
+        'price': 2.00,
+        'picture': './img/donut/donut_5.png',
+    },
+    
+    {
+        "name": "Peanut Butter",
+        "recepte": ["Erdnussbutterfüllung mit einer Schicht Fruchtgelee oben drauf"],
+        "price": 3.00,
+        "picture": "./img/donut/donut_6.png"
+    },
+    {
+        "name": "Tropical Paradise",
+        "recepte": ["Mango- und Passionsfruchtfüllung, garniert mit Kokosraspeln"],
+        "price": 2.50,
+        "picture": "./img/donut/donut_7.png"
     },
 ]
 
@@ -22,12 +54,12 @@ let orderMenuArray = [];
 let orderAmountArray = [];
 let orderPriceArraySolo = [];
 let orderPriceArrayTotal = [];
-let deliverPrice = 5;
+let deliverPrice = 3.50;
+let minimumOrderValue = 10;
 
 function render() {
     let menuID = document.getElementById('menuID');
     menuID.innerHTML = '';
-
     for (let i = 0; i < menuArray.length; i++) {
         menuLoad(i);
     }
@@ -38,32 +70,51 @@ function menuLoad(i) {
     const element = menuArray[i]
 
     menuID.innerHTML += /*html*/`
-    <div>
-        Name: ${element['name']} <br>
-        Zutaten: ${element['recepte']} <br>
-        Preis: ${element['price']} €  <br> 
-        Bestellung: <input id="amountID${i}" type="number" min="1" max ="10" value="1"><br>
-        <button onclick="addToBasket(${i})"> Kaufen </button> 
-        <br> <br> <br>   
-    </div> 
-`
+    <div class="menuStyle"> 
+        <div class="menuStyleSub">
+           <h3> ${element['name']} </h3> 
+            <p>${element['recepte']}<p>
+            <b> ${element['price'].toFixed(2)} € </b> <!-- .toFixed(2) lässt zwei hintere Dezimalzahl erscheinen! -->  
+            <input class="inputStyle" id="amountID${i}" type="number" min="1" max ="99" value="1">                      
+        </div> 
+        <div class="dishInteract"> 
+            <img class="cursor dishInteractButtonFromMenu" src="./img/minus.png" onclick="reduceAmount(${i})">
+            
+            <img class="productImgStyle" src="${element['picture']}" alt="${element['name']}">     
+
+            <img class="cursor dishInteractButtonFromMenu" src="./img/plus.png" onclick="addToBasket(${i})"> 
+        </div>
+    </div>
+    `;
+
+    showBasketContainer();
 }
 
-function getMenuIndex(menu) {
-    let indexOf = orderMenuArray.indexOf(menu);
-    return indexOf;
+
+function showBasketContainer() {
+    document.getElementById('donutBasketContainerID').innerHTML = /*html*/`
+    <div class="firstBasket" id="basketID" >
+        <h2 class="headbasket"> Warenkorb </h2>
+            <img src="./img/basketBigImg.png" class="basketBigStyleImg">
+        <h2> Fülle deinen Warenkorb</h2>
+             <span> Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</span>
+    </div>
+    <div class="secondBasket" id="secondBasketWithDeleteID">
+        <div id="basketNoticeID"> </div>
+        <div id="payButtonID"> </div>
+    </div>
+    `;
 }
 
-function getAmountFromInput(input) {
-    let amountValue = +document.getElementById("amountID" + input).value;
-    return amountValue;
-}
 
+function reduceAmount(i) {
+    let index = orderMenuArray.indexOf(menuArray[i].name);
+    downNewAmount(index);
+}
 
 function addToBasket(index) {
     let amount = getAmountFromInput(index);
     let getMenuIndexOf = getMenuIndex(menuArray[index]['name']);
-
     if (getMenuIndexOf == -1) {
         orderAmountArray.push(amount);
         orderMenuArray.push(menuArray[index]['name']);
@@ -71,47 +122,58 @@ function addToBasket(index) {
         orderPriceArrayTotal.push(price);
         orderPriceArraySolo.push(menuArray[index]['price']);
         const priceTotal = (price + deliverPrice)
-
     } else {
         orderAmountArray[getMenuIndexOf] += amount;
         orderPriceArrayTotal[getMenuIndexOf] = orderAmountArray[getMenuIndexOf] * menuArray[index]['price'];
     }
-
     document.getElementById("amountID" + index).value = 1;
     renderBasket();
     showBasketBill();
     showBasketNotice();
-
 }
 
 
+function getAmountFromInput(input) {
+    let amountValue = +document.getElementById("amountID" + input).value;
+    return amountValue;
+}
+
+function getMenuIndex(menu) {
+    let indexOf = orderMenuArray.indexOf(menu);
+    return indexOf;
+}
+
 function renderBasket() {
-    let basket = document.getElementById('basket');
+    let basket = document.getElementById('basketID');
     basket.innerHTML = '';
-
     for (let indexBasket = 0; indexBasket < orderMenuArray.length; indexBasket++) {
-
         basket.innerHTML += /*html*/ `
-            <div> 
-                <div class="basketContainerStyle"> 
-                 Gericht: ${orderMenuArray[indexBasket]}  <img onclick="removeFromBasket(${indexBasket})" src="./img/close.png" class="cursor" ><br><br>
-                 Zwischenpreis: ${orderPriceArrayTotal[indexBasket]} € <br>  <br>                    
-                 Gesamtanzahl: ${orderAmountArray[indexBasket]} <br> <br>                                 
+        <div class="dishInBasketContainer">
+            <div class="disnInBasketContainer_2">
+                <div class="dishInBasketHead">  
+                    <div>
+                        <span class="dishAmount"> ${orderAmountArray[indexBasket]} </span> <!--Gesamtanzahl-->
+                        <span class="dishName"> ${orderMenuArray[indexBasket]}  </span> <!--Gericht--> 
+                    </div>
 
-                    <div class="basketStyle">
-                        <div class="button"> 
-                            <img class="cursor" src="./img/down.png" onclick="downNewAmount(${indexBasket})">
-                        </div>
-                        <div id="amountBasket${indexBasket}"> 
-                            ${orderAmountArray[indexBasket]} 
-                        </div>
-                        <div class="button">
-                             <img class="cursor" src="./img/up.png" onclick="upNewAmount(${indexBasket})"> 
-                        </div> <br>
+                    <div>
+                        ${orderPriceArrayTotal[indexBasket].toFixed(2)} € <!--Zwischenpreis-->   
+                    </div>  
+                </div>  
+
+                <div class="dishInBasketAmount">   
+                    <div class="dishInBasketAmountValue">
+                        <img class="cursor dishInteractButtonFromBasket" src="./img/minus.png" onclick="downNewAmount(${indexBasket})"> 
+                        <span> ${orderAmountArray[indexBasket]} </span>
+                        <img class="cursor dishInteractButtonFromBasket" src="./img/plus.png" onclick="upNewAmount(${indexBasket})"> 
+                    </div>
+                    <div class="closeImg">
+                        <img onclick="closeBasket(${indexBasket})" src="./img/close.png" class="cursor closeImg" >   
                     </div>
                 </div>
-            </div> <br> <br> <br>
-        `;
+            </div>
+        </div> 
+    `;
     }
 }
 
@@ -121,55 +183,75 @@ function showBasketBill() {
         totalOrderSum += orderPriceArrayTotal[prices];
     }
     totalOrderAndDeliverPrice = totalOrderSum + deliverPrice;
-
-    let bill = document.getElementById('basketBillID');
+    let bill = document.getElementById('payButtonID');
     bill.innerHTML = /*html*/ `
-    <div> 
-        <h2>Zwsichen Preis aller Gerichte: ${totalOrderSum}€ </h2> 
-        <h2>Lieferkosten: + ${deliverPrice}€ </h2>
-        <h1>Gesamtpreis: ${totalOrderAndDeliverPrice}€</h1> 
+    <div class="basketBillContainer"> 
+        <div class="basketBillContainerSubtotal">
+            <span> Zwischensumme </span> 
+            <span> ${totalOrderSum.toFixed(2)}€ </span> 
+        </div>
+
+        <div class="basketBillContainerDeliver">
+            <span> Lieferkosten </span>
+            <span> ${deliverPrice.toFixed(2)}€ </span>
+        </div>
+
+        <div class="basketBillContainerTotal">
+            <span> Gesamtpreis </span>
+            <span>  ${totalOrderAndDeliverPrice.toFixed(2)}€ </span>
+        </div> 
     </div>
     `;
 }
 
-function showBasketNotice() {
+function showBasketNotice(i) {
     let bill = document.getElementById('basketNoticeID');
+    let stillToPay = 0;
+    stillToPay = minimumOrderValue - totalOrderAndDeliverPrice;
+
     bill.innerHTML = /*html*/ `
     <div class="basketNoticeContainer"> 
-        <div class="payOrderStyle" id="payOrderID"> 
-             Mindest-Bestellwert ist noch nicht erreicht 
-        </div> <br>
-
         <div class="payOrderStyleNotice">  
-             1. Der Mindestbestellwert liegt bei 40 € <div id="checklistOrder_1"> </div> <br>
-             2. Bei einem Bestellwert ab 70€ ist die Lieferung frei Haus <div id="checklistFreeDeliver_2"> </div> <br>
-             3. Bei einem Bestellwert ab 100€ bekommen Sie eine Flasche erlesenen Qualitätswein gratis dazu <div id="checklistVineForFree_1"> </div> <br>
+            <div class="payOrderSpan"><span > 1. Der Mindestbestellwert liegt bei ${minimumOrderValue} <span id="checklistOrder_1"> </span> </span></div>
+            <div class="payOrderSpan"><span> 2. Kostenloser Versand ab 25€ <span id="checklistFreeDeliver_2"> </span> </span></div>
+            <div> 3. Gratis Donut Express T-Shirt ab 40€ </span> <span id="checklistForFree_1"> </span> </span></div>
+        </div>
+    </div>
+    </div>
+    <div class="payOrderStyle_1" id="payOrderID"> 
+        <div> 
+            <span> Benötigter Betrag, um dem Mindestbestellwert zu erreichen</span>
+        </div>
+        <div> 
+            <b class="payOrderStillToPay"> ${stillToPay.toFixed(2)}€</b>
         </div>
     </div>
     `;
     calculateBasketNotice();
 }
 
+
+
 function calculateBasketNotice(index) {
-    if (totalOrderAndDeliverPrice >= 40) {
+    if (totalOrderAndDeliverPrice >= 10) {
         document.getElementById('payOrderID').innerHTML = /*html*/ `
-        <div> BEZAHLEN </div>`;
+        <div class="payOrderStyle_2"> BEZAHLEN </div>`;
         document.getElementById('checklistOrder_1').innerHTML = /*html*/ `
-        <img src="./img/correct.png"> `;
+        <img class="correctImg" src="./img/correct.png"> `;
+        var payOrderIDElement = document.getElementById('payOrderID');
+        payOrderIDElement.setAttribute('style', 'background-color: white; margin-bottom: 0px; margin-top: 0px;'); /* Background Style von Bezahlen !!!! */
     }
 
-    if (totalOrderAndDeliverPrice >= 70) {
+    if (totalOrderAndDeliverPrice >= 25) {
         document.getElementById('checklistFreeDeliver_2').innerHTML = /*html*/ `
-        <img src="./img/correct.png"> `;
+        <img class="correctImg" src="./img/correct.png"> `;
     }
 
-    if (totalOrderAndDeliverPrice >= 100) {
-        document.getElementById('checklistVineForFree_1').innerHTML = /*html*/ `
-        <img src="./img/correct.png"> `;
+    if (totalOrderAndDeliverPrice >= 40) {
+        document.getElementById('checklistForFree_1').innerHTML = /*html*/ `
+        <img class="correctImg" src="./img/correct.png"> `;
     }
 }
-
-
 
 function upNewAmount(index) {
     let getMenuIndexOf = getMenuIndex(orderMenuArray[index]);
@@ -189,35 +271,30 @@ function downNewAmount(index) {
         showBasketBill();
         showBasketNotice();
     } else {
-        removeFromBasket(index) 
-    }                           
-
+        closeBasket(index)
+    }
 }
 
-function removeFromBasket(index) {
+function closeBasket(index) {
     let getMenuIndexOf = getMenuIndex(orderMenuArray[index]);
 
-    if (getMenuIndexOf !== -1) { 
+    if (getMenuIndexOf !== -1) {
         orderAmountArray.splice(getMenuIndexOf, 1);
         orderMenuArray.splice(getMenuIndexOf, 1);
         orderPriceArrayTotal.splice(getMenuIndexOf, 1);
         orderPriceArraySolo.splice(getMenuIndexOf, 1);
-
         renderBasket();
         showBasketBill();
     }
 
-    if(orderAmountArray.length === 0) { // damit sagst du wenn dein Array Leer ist, könntest auch sagen "orderAmountArray < 1" führt zum selben
-        let basketNoticeDeleteID = document.getElementById('basketNoticeDeleteID');
-        basketNoticeDeleteID.innerHTML = `
-        <div id="basketBillID"> </div>
+    if (orderAmountArray.length === 0) {
+        let secondBasketWithDeleteID = document.getElementById('secondBasketWithDeleteID');
+        secondBasketWithDeleteID.innerHTML = /*html*/`
+        <div id="payButtonID"> </div>
         <div id="basketNoticeID"> </div>
-        `
-        ;}
+        `;
+        showBasketContainer();
+    }
 }
 
-// @Gelöst:
-// downNewAmount Funktion ist nun korrekt, wenn du unter 0 gehst führt es else Funktion aus
-// Auch die Elemente "basketBillID" "basketNoticeID" wie in IF Abfrage 210 werden ausgeführt
-// da du bei downNewAmount die Funktionen nun nicht allgemein drinne hast sondern in If Abfrage
-// wenn du Sie Außerhalb tust dann führt es auch in Else aus dadurch wird es wieder gerendet (Siehe Aufgabe 23_Lieferando.js wenn du nicht versteht was ich meine)
+
